93bb1764ae7edadfc5b5e68c230f92f7cd7f32d7
Sankalp Mukim <sankalpmukim@gmail.com>
2022-03-18 08:37:10 +0530

    build check

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,28 +1,9 @@
-import { ReactNode, useContext } from "react";
+import { useContext } from "react";
 import { UserContext } from "../userstore";
-import Requester from "./Requester";
-
-// promise that resolves after 3 seconds
-const promise: () => Promise<string> = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => reject("Hello"), 3000);
-  });
-};
-
-const SuccessComponent: (response: string) => ReactNode = (
-  response: string
-) => {
-  return <div>{response}</div>;
-};
 
 function Content() {
   const { username } = useContext(UserContext);
-  return (
-    <>
-      This is the username: {username}
-      <Requester request={promise} successComponent={SuccessComponent} />
-    </>
-  );
+  return <>This is the username: {username}</>;
 }
 
 export default Content;
